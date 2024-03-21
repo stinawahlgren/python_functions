75ba604baf1795948e3e248b0bb2351ae2e3dda8
Stina Wahlgren <stina.wahlgren@gu.se>
2024-03-21 15:58:21 +0100

    Grid while keeping one dimension intact

--- xrtools.py
+++ xrtools.py
@@ -7,7 +7,7 @@ from .misc import get_edges
 def has_same_coords(da1, da2):
     """
     Check if two xr.DataArrays have the same coordinates (name and values). 
-    The coordinates does not have to come in the same order.
+    The coordinates do not have to come in the same order.
     """
     output = True
     
@@ -32,11 +32,84 @@ def grid_data_1d(da_x, da_y, x_grid, label = None):
     
     Returns an xarray dataset with mean, count and standard deviation for grid cells. 
     If label is given, the variables will be called <label>_mean, <label>_count, <label>_std 
+    """     
+
+    if label is None:
+        prefix = ""
+    else:
+        prefix = f"{label}_"
+ 
+    # Compute statistics   
+    mean  = da_from_binned_statistic_1d(da_x, da_y, x_grid, 'mean'
+                                        ).to_dataset(name = prefix + 'mean')
+    count = da_from_binned_statistic_1d(da_x, da_y, x_grid, 'count'
+                                        ).to_dataset(name = prefix + 'count')
+    std   = da_from_binned_statistic_1d(da_x, da_y, x_grid, 'std'
+                                        ).to_dataset(name = prefix + 'std')
+
+    return mean.merge(std).merge(count)
+
+
+def da_from_binned_statistic_1d(da_x, da_y, x_grid, statistic):
+    """
+    Parameters:
+        da_x : An xarray DataArray with values to be binned
+        da_y : An xarray DataArray with the data on which the statistic will be computed.
+               Can have one more dimension than da_x, but the common dimensions must have
+               same names and coordinate values as da_x.
+        x_grid : one-dimensional xarray DataArray with center points of the grid
+        statistic : passed to scipy.stats.binned_statistic (e.g 'mean', 'count', 'std')
+        
+    Returns:
+        An xarray DataArray with statistic of the binned data
+    """
+
+    different_dimensions = set(da_y.dims) - set(da_x.dims)
+    
+    grid_dim = list(x_grid.dims)[0]
+    
+    if len(different_dimensions) == 0:
+        dims   = grid_dim
+        coords = {grid_dim: x_grid.values}      
+        data   = get_binned_statistics(da_x, da_y, x_grid, statistic).statistic
+        
+    elif len(different_dimensions) == 1:
+        keep_dim = list(different_dimensions)[0]
+        dims   = (grid_dim, keep_dim)
+        coords = {grid_dim: x_grid.values,
+                  keep_dim: da_y[keep_dim].values}
+        
+        len_keep_dim = len(da_y[keep_dim].values) 
+        data = np.empty([len(x_grid.values), len_keep_dim])
+        for i in range(len_keep_dim):
+            data[:,i] = get_binned_statistics(da_x, da_y.isel({keep_dim : i}), x_grid, statistic).statistic
+    
+    else:
+        raise ValueError("da_y can have at most one more dimension than da_x")
+            
+    # Create DataArray
+    da = xr.DataArray(data, 
+                      dims   = dims,
+                      coords = coords)
+    return da
+
+
+def get_binned_statistics(da_x, da_y, x_grid, statistic):
+    """
+    Parameters:
+        da_x : An xarray DataArray with values to be binned
+        da_y : An xarray DataArray with the data on which the statistic will be computed.
+               Must have same dimensions and coordinate values as da_x
+        x_grid : one-dimensional xarray DataArray with center points of the grid
+        statistic : passed to scipy.stats.binned_statistic (e.g 'mean', 'count', 'std')
+        
+    Returns:
+        Output directly from scipy.stats.binned_statistic
     """
     # Make sure grid is 1D:    
     if len(x_grid.dims) != 1:
         raise ValueError('Grid must be one-dimensional!')
-    
+        
     # Make sure that x and y have same coordinates
     if not has_same_coords(da_x, da_y):
         raise ValueError("da_x and da_y don't match")
@@ -51,41 +124,6 @@ def grid_data_1d(da_x, da_y, x_grid, label = None):
     x = x[~is_nan]
     y = y[~is_nan]
     
-    # Compute statistics   
-    if label is None:
-        prefix = ""
-    else:
-        prefix = f"{label}_"
-    
-    mean  = da_from_binned_statistic_1d(x, y, x_grid, 'mean'
-                                        ).to_dataset(name = prefix + 'mean')
-    count = da_from_binned_statistic_1d(x, y, x_grid, 'count'
-                                        ).to_dataset(name = prefix + 'count')
-    std   = da_from_binned_statistic_1d(x, y, x_grid, 'std'
-                                        ).to_dataset(name = prefix + 'std')
-
-    return mean.merge(std).merge(count)
-
-
-def da_from_binned_statistic_1d(x, y, x_grid, statistic):
-    """
-    Parameters:
-        x : (N,) array_like. A sequence of values to be binned
-        y : (N,) array_like. The data on which the statistic will be computed
-        x_grid : one-dimensional xarray DataArray with center points of the grid
-        statistic : passed to scipy.stats.binned_statistic (e.g 'mean', 'count', 'std')
-        
-    Returns:
-        An xarray DataArray with statistic of the binned data
-    """
     # Compute statistics
     bin_edges = get_edges(x_grid.values)  
-    data = binned_statistic(x, y,
-                            statistic = statistic,
-                            bins = bin_edges).statistic
-    
-    # Create DataArray
-    da = xr.DataArray(data, 
-                      dims   = x_grid.dims[0],
-                      coords = {x_grid.dims[0]: x_grid.values})
-    return da
+    return binned_statistic(x, y, statistic = statistic, bins = bin_edges)
