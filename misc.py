7f90381f5c9bb1ae3a4fc61e962643b224bf3409
Stina Wahlgren <stina.wahlgren@gu.se>
2024-03-25 15:37:08 +0100

    tiny function for comparing columns in matrix

--- misc.py
+++ misc.py
@@ -6,3 +6,17 @@ def get_edges(centers):
     first = centers[0] - (centers[1]-centers[0])/2
     last  = centers[-1] + (centers[-1]-centers[-2])/2
     return np.concatenate([[first], mid, [last]])
+
+
+def all_columns_equal(a):
+    """
+    Check if all columns in a are equal
+    
+    Parameters:
+        a: (n,m) numpy.ndarray
+        
+    Returns:
+        True/False
+    """
+    m = a.shape[1]
+    return ~(a - np.tile(a[:,0], (m,1)).T).any()
